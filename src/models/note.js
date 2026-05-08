6424152a3faf80f8e2ce201741e11b370f888247
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-05-08 10:19:53 +0300

    add mongodb crud operations

---
+++ src/models/note.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  { timestamps: true },
+);
+
+export const Note = model('Note', noteSchema);
