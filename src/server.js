cc5ce04b3bddf54d4cb96f76066ab18815704a06
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-05-10 21:19:23 +0300

    add auth structure

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(authRoutes);
 
 app.use(notesRoutes);
 
